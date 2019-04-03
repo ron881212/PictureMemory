f3af14a41ad835be92b86a49a2db983a8a345295
Ronald Glover <ron881212@gmail.com>
2019-04-03 04:00:40 -0400

    Updates

---
+++ precache-manifest.3ebaef5ecbefd197ce1fbbd23cf08fd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62bf96e6f46e1a1b31bd",
+    "url": "/PictureMemory/static/js/runtime~main.1a53c5ca.js"
+  },
+  {
+    "revision": "3d17462deefa512af3c4",
+    "url": "/PictureMemory/static/js/main.93e98d4c.chunk.js"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/PictureMemory/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "3d17462deefa512af3c4",
+    "url": "/PictureMemory/static/css/main.b8ac03c2.chunk.css"
+  },
+  {
+    "revision": "15948c557086714ebaf108a435c51183",
+    "url": "/PictureMemory/index.html"
+  }
+];
